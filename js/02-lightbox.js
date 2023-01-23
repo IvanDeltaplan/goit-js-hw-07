05b511764698a7ff07d2588f7d3450900b0d43b5
IvanDeltaplan <ivannikolaev777@gmail.com>
2023-01-23 19:00:49 +0200

    updated

---
+++ js/02-lightbox.js
@@ -0,0 +1,39 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+console.log(galleryItems);
+const galleryGrid = document.querySelector(".gallery");
+
+const galleryStr = galleryItems
+  .map((item) => {
+    // console.log(item.original)
+    return ` <div class= gallery__item>
+          <a class= gallery__link href= ${item.original} >
+            
+          <img
+          class= gallery__image
+          src= ${item.preview}
+          data-source= ${item.original}
+          alt= ${item.description} 
+            />
+    </a>
+    </div>
+        `;
+  })
+  .join("");
+
+galleryGrid.addEventListener("click", onClick);
+function onClick(event) {
+  event.preventDefault();
+}
+galleryGrid.insertAdjacentHTML("beforeend", galleryStr);
+
+let gallery = new SimpleLightbox(".gallery a");
+gallery.on("show.simplelightbox", function () {
+  // Do something…
+});
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
